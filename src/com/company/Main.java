485e4b09aff367f82ac15258ab51f02501867bfd
Vu-ngoc-huy-1981 <ngochuy.thpttruongchinh@ninhthuan.edu.vn>
2021-03-25 21:23:52 +0700

    toan tu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,19 @@ System.out.println("Hello Word");
 // 1. tạo thư mục .git
 
         //2. kiểu dữ liệu
+//        String name = " Vũ Ngọc Huy";
+//        int age = 15;
+//        float s1 = 1.5f;
+//        double s2 = 1.5;
+//        char s3 = 'a';
+//        4: toán tử
+//        a+=1;
+int a =5;
+int b= 5;
+int ketqua = a-- + --b - --b - --a + b-- - --a;
+System.out.println(ketqua);
+
+
 
     }
 }
